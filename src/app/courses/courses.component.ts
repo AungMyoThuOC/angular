5fbb807f124a448b8ad1238097df626e77a530b1
AungMyoThuOC <thua40976@gmail.com>
2023-03-16 13:10:22 +0630

    add courses

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
   constructor() { }
+
+  title : string = "Onii Chan"
+
+  courseid =4;
+
+  red : boolean = true;
+
+  imgurl : string = 'https://wallpaperaccess.com/full/386762.jpg'
     
     ngOnInit(): void {
-      
+      this.courseid = 4;
+      this.title = "Onii Chan baka"
     }
+    
   }
